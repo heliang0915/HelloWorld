3329c88099be940a98b0a78d0aaaa466d5a6d8d0
heliang0915 <598481337@qq.com>
2016-06-22 22:45:59 +0800

     提交代码

--- app/component/App.js
+++ app/component/App.js
@@ -3,17 +3,22 @@ import {StyleSheet,View,Image,Text,ScrollView,Dimensions} from 'react-native';
 import InfoList from './InfoList.js';
 import Form from './Form.js';
 // import Refresh from './Refresh.js';
-import Header from './Demo/Header.js';
-import NewsList from './Demo/NewsList.js';
+// import Header from './Demo/Header.js';
+// import NewsList from './Demo/NewsList.js';
+// import TabBar from './Demo/TabBar.js';
+// import Animate from './Animate.js';
+import Nav from './Nav.js';
+import TextIn from './TextIn.js';
+
+// import IndexPage from '../Page/IndexPage.js'
+import StartPager from './StartPager.js';
+import Banner from './Banner.js';
 
 class App extends Component{
 
     render(){
         return(
-            <ScrollView contentContainerStyle={{flex:1,height:Dimensions.get("window").height}}>
-               <Header />
-               <NewsList />
-           </ScrollView>
+            <Nav defaultComponent={StartPager} />
         )
     }
 }
